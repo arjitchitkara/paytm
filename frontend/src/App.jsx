318545820d3b7a490f01bc933349f04807758589
Arjit <arjitchitkara004@gmail.com>
2024-06-05 19:11:55 +0530

    frontend fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./Signup";
-import Signin from "./Signin";
-import Dashboard from "./Dashboard";
-import SendMoney from "./SendMoney";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
@@ -16,7 +20,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
